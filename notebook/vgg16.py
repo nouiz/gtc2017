afd28aa44b92f8c6d44490b27e1b792bd4dc40aa
Frederic Bastien <nouiz@nouiz.org>
2017-04-27 12:46:31 -0400

    First version of the vgg16 notebook

--- notebook/vgg16.py
+++ notebook/vgg16.py
@@ -6,14 +6,20 @@
 # Download pretrained weights from:
 # https://s3.amazonaws.com/lasagne/recipes/pretrained/imagenet/vgg16.pkl
 
+import io
+
+import matplotlib.pyplot as plt
+import numpy as np
+import skimage.transform
+
 from lasagne.layers import InputLayer
 from lasagne.layers import Conv2DLayer as ConvLayer
 from lasagne.layers import DenseLayer
 from lasagne.layers import NonlinearityLayer
 from lasagne.layers import DropoutLayer
 from lasagne.layers import Pool2DLayer as PoolLayer
-#from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
 from lasagne.nonlinearities import softmax
+from lasagne.utils import floatX
 
 
 def build_model():
@@ -59,3 +65,39 @@ def build_model():
     net['prob'] = NonlinearityLayer(net['fc8'], softmax)
 
     return net
+
+
+def prep_image(url, mean_image):
+    ext = url.split('.')[-1]
+    im = plt.imread(io.BytesIO(open(url).read()), ext)
+    # Resize so smallest dim = 256, preserving aspect ratio
+    h, w, _ = im.shape
+
+    if h < w:
+        new_shape = (256, w*256/h)
+    else:
+        new_shape = (h*256/w, 256)
+    im = skimage.transform.resize(im, new_shape,
+                                  preserve_range=True,
+                                  mode='constant')
+
+    # Central crop to 224x224
+    h, w, _ = im.shape
+    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
+    # If all values are bellow 1, we should keep it as float
+    # otherwise, we need to convert it to uint8 to display well
+    if im.max() <= 1:
+        rawim = im
+    else:
+        rawim = np.copy(im).astype('uint8')
+
+    # Shuffle axes to c01
+    im = np.swapaxes(np.swapaxes(im, 1, 2), 0, 1)
+
+    # Drop the alpha channel, VGG16 don't support it
+    im = im[:3]
+
+    # Convert to BGR
+    im = im[::-1, :, :]
+
+    return rawim, floatX(im[np.newaxis])
